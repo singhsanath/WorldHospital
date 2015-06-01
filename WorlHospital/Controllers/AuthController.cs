c457de18e4775689ddf584ab07780e957cb0a2ff
sanathsingh <sanathsingh06@gmail.com>
2015-06-01 11:33:19 +0530

    First Comment in World Hospital

---
+++ WorlHospital/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorlHospital.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+
+        [AllowAnonymous]
+        [HttpGet]
+       public ActionResult Login()
+        {
+            return View( );
+
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Registration()
+       {
+           return View( );
+       }
+        [AllowAnonymous]
+        [HttpPost]
+        public JsonResult Registration(string Client_Data)
+        {
+            return Json( Client_Data );
+        }
+
+        public JsonRequestBehavior Login( string Client_login )
+        {
+            return JsonRequestBehavior.AllowGet;
+        }
+
+
+    }
+}
---
+++ WorlHospital/Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorlHospital.Controllers
+{
+    public class LocationController : Controller
+    {
+        //
+        // GET: /Location/
+
+        public JsonResult Country()
+        {
+           
+            return Json( "countries" );
+        }
+        public JsonResult states(string country)
+        {
+            return Json( "states object" );
+        }
+    }
+}
